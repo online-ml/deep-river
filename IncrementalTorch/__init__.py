8775caba2aa8800a1a27f212798738c3b787e9ee
Cedric Kulbach <kulbach@fzi.de>
2021-11-22 12:59:08 +0100

    refactor imports

--- IncrementalTorch/__init__.py
+++ IncrementalTorch/__init__.py
@@ -1,16 +1,14 @@
 from . import (
-    OnlineTorch,
-    OnlineKeras,
-    config,
-    utils,
-    tracks
+    anomaly,
+    classification,
+    regression,
+    utils
 )
 from .__version__ import __version__  # noqa: F401
 
 __all__ = [
-    "OnlineTorch",
-    "OnlineKeras",
-    "config",
-    "utils",
-    "tracks"
+    "anomaly",
+    "classification",
+    "regression"
+    "utils"
 ]
