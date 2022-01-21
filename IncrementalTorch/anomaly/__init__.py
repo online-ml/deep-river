0d9605605c91e3e6f5e681f5497b717852970025
Cedric Kulbach <kulbach@fzi.de>
2022-01-21 09:42:18 +0100

    Skip nn function in init

--- IncrementalTorch/anomaly/__init__.py
+++ IncrementalTorch/anomaly/__init__.py
@@ -1,6 +1,5 @@
 from .anomaly import Autoencoder, AdaptiveAutoencoder, VariationalAutoencoder, RollingWindowAutoencoder, \
     SkipAnomAutoencoder, BasicAutoencoder
-from .nn_builder import get_fc_autoencoder
 from .postprocessing import ScoreStandardizer
 
 __all__ = [
@@ -10,6 +9,5 @@ __all__ = [
     "VariationalAutoencoder",
     "RollingWindowAutoencoder",
     "SkipAnomAutoencoder",
-    "get_fc_autoencoder",
     "BasicAutoencoder",
 ]
