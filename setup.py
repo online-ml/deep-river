7b417179bef86fe76a101c05ba01fed1c3d2f89c
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-04-24 18:24:29 +0200

    Add .gitignore

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ base_packages = [
     "torch==1.10.2",
     "vaex==4.8.0",
     "pandas~=1.3.2",
-    "numpy==1.22.2",
+    "numpy>=1.21.6",
     "river~=0.10.1",
     "tqdm~=4.61.2",
     "pytest==7.0.1",
