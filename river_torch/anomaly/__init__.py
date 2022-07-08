d7336e39490f00b0060bc50245ed86116904e4d6
Lucas Cazzonelli <cazzonelli@fzi.de>
2022-07-08 16:22:21 +0200

    refactor: :art: merge anomaly score scalers
    Merge anomaly score scalers with different metric computation approaches into single classes.
    
    14-merge-anomalyscalers

--- river_torch/anomaly/__init__.py
+++ river_torch/anomaly/__init__.py
@@ -2,19 +2,15 @@ from .base import AnomalyScaler, Autoencoder
 from .probability_weighted_ae import ProbabilityWeightedAutoencoder
 from .rolling_ae import RollingWindowAutoencoder
 from .scaler import (
-    AdaptiveMeanScaler,
-    AdaptiveStandardScaler,
     MeanScaler,
     MinMaxScaler,
-    RollingMeanScaler,
-    RollingMinMaxScaler,
-    RollingStandardScaler,
     StandardScaler,
 )
 from .variational_ae import VariationalAutoencoder
 
 __all__ = [
     "Autoencoder",
+    "AnomalyScaler",
     "VariationalAutoencoder",
     "RollingWindowAutoencoder",
     "ProbabilityWeightedAutoencoder",
