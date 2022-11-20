b340cce40b38cf32e8b985fc0d3d6f0d04064ad4
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-11-20 19:28:53 +0100

    FIX hooks

--- river_torch/utils/hooks.py
+++ river_torch/utils/hooks.py
@@ -1,6 +1,11 @@
+from typing import List
+
+from torch import nn
+
+
 class ForwardOrderTracker:
     def __init__(self) -> None:
-        self.ordered_modules = []
+        self.ordered_modules: List[nn.Modules] = []
 
     def __call__(self, module, input, output):
         if list(module.parameters()) and not list(module.children()):
