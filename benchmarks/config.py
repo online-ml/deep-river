fd8e7154704da9d72d350bdd33db430b515e5dce
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-12-28 10:52:16 +0100

    refactor rendering

--- benchmarks/config.py
+++ benchmarks/config.py
@@ -25,6 +25,16 @@ MODELS = {
         )
     },
     "Multiclass classification": {
+        "Torch Logistic Regression": (
+            preprocessing.StandardScaler()
+            | TorchClassifier(
+                module=TorchLogisticRegression,
+                loss_fn="binary_cross_entropy",
+                optimizer_fn="adam",
+                is_class_incremental=True,
+                lr=LEARNING_RATE
+            )
+        ),
         "Torch MLP": (
             preprocessing.StandardScaler()
             | TorchClassifier(
@@ -35,12 +45,61 @@ MODELS = {
                 lr=LEARNING_RATE
             )
         ),
+        "Torch LSTM": (
+            preprocessing.StandardScaler()
+            | TorchRollingClassifier(
+                module=TorchLSTMClassifier,
+                loss_fn="binary_cross_entropy",
+                optimizer_fn="adam",
+                is_class_incremental=True,
+                lr=LEARNING_RATE,
+                window_size=20,
+                append_predict=False
+            )
+        ),
         "[baseline] Last Class": dummy.NoChangeClassifier(),
     },
     "Regression": {
+        "Torch Linear Regression": (
+            preprocessing.StandardScaler()
+            | TorchRegressor(
+                module=TorchLinearRegression,
+                loss_fn="mse",
+                optimizer_fn="adam",
+                lr=LEARNING_RATE,
+            )
+        ),
         "Torch MLP": (
             preprocessing.StandardScaler()
-            | linear_model.LinearRegression(optimizer=optim.SGD(LEARNING_RATE))
+            | TorchRegressor(
+                module=TorchMLPRegressor,
+                loss_fn="mse",
+                optimizer_fn="adam",
+                lr=LEARNING_RATE,
+            )
         ),
+        "River MLP": preprocessing.StandardScaler()
+        | neural_net.MLPRegressor(
+            hidden_dims=(5,),
+            activations=(
+                neural_net.activations.ReLU,
+                neural_net.activations.ReLU,
+                neural_net.activations.Identity,
+            ),
+            optimizer=optim.SGD(1e-3),
+            seed=42,
+        ),
+        "Torch LSTM": (
+            preprocessing.StandardScaler()
+            | TorchRollingRegressor(
+                module=TorchLSTMRegressor,
+                loss_fn="mse",
+                optimizer_fn="adam",
+                lr=LEARNING_RATE,
+                window_size=20,
+                append_predict=False
+            )
+        ),
+        "[baseline] Mean predictor": dummy.StatisticRegressor(stats.Mean()),
     },
 }
