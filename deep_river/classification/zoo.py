e18b7a6c57d13b5cf453444f7de00a4e64b01a13
Cedric Kulbach <cedric.kulbach@googlemail.com>
2025-03-01 17:37:24 +0100

    Refactor classification zoo for initialized models

--- deep_river/classification/zoo.py
+++ deep_river/classification/zoo.py
@@ -294,9 +294,9 @@ class LogisticRegressionInitialized(ClassifierInitialized):
     """
 
     class LRModule(nn.Module):
-        def __init__(self):
+        def __init__(self, n_features: int):
             super().__init__()
-            self.dense0 = nn.Linear(10, 1)
+            self.dense0 = nn.Linear(in_features=n_features, out_features=1)
             self.softmax = nn.Softmax(dim=-1)
 
         def forward(self, x, **kwargs):
@@ -305,6 +305,7 @@ class LogisticRegressionInitialized(ClassifierInitialized):
 
     def __init__(
         self,
+        n_features: int = 10,
         loss_fn: Union[str, Callable] = "binary_cross_entropy_with_logits",
         optimizer_fn: Union[str, Type[optim.Optimizer]] = "sgd",
         lr: float = 1e-3,
@@ -315,8 +316,101 @@ class LogisticRegressionInitialized(ClassifierInitialized):
         seed: int = 42,
         **kwargs,
     ):
-        # Initialize the parent class with the passed module
-        module = LogisticRegressionInitialized.LRModule()
+        self.n_features = n_features
+        module = LogisticRegressionInitialized.LRModule(n_features=n_features)
+        if "module" in kwargs:
+            del kwargs["module"]
+        super().__init__(
+            module=module,
+            loss_fn=loss_fn,
+            optimizer_fn=optimizer_fn,
+            output_is_logit=output_is_logit,
+            is_class_incremental=is_class_incremental,
+            is_feature_incremental=is_feature_incremental,
+            device=device,
+            lr=lr,
+            seed=seed,
+            **kwargs,
+        )
+
+    @classmethod
+    def _unit_test_params(cls):
+        """
+        Returns a dictionary of parameters to be used for unit testing the
+        respective class.
+        """
+
+        yield {
+            "loss_fn": "binary_cross_entropy_with_logits",
+            "optimizer_fn": "sgd",
+            "is_feature_incremental": True,
+            "is_class_incremental": True,
+        }
+
+
+class MultiLayerPerceptronInitialized(ClassifierInitialized):
+    """
+    Logistic Regression model for classification.
+
+    Parameters
+    ----------
+    loss_fn : str or Callable
+        Loss function to be used for training the wrapped model.
+    optimizer_fn : str or Callable
+        Optimizer to be used for training the wrapped model.
+    lr : float
+        Learning rate of the optimizer.
+    output_is_logit : bool
+        Whether the module produces logits as output. If true, either
+        softmax or sigmoid is applied to the outputs when predicting.
+    is_class_incremental : bool
+        Whether the classifier should adapt to the appearance of previously unobserved classes
+        by adding an unit to the output layer of the network.
+    is_feature_incremental : bool
+        Whether the model should adapt to the appearance of previously features by
+        adding units to the input layer of the network.
+    device : str
+        Device to run the wrapped model on. Can be "cpu" or "cuda".
+    seed : int
+        Random seed to be used for training the wrapped model.
+    **kwargs
+        Parameters to be passed to the `build_fn` function aside from `n_features`.
+    """
+
+    class MLPModule(nn.Module):
+        def __init__(self, n_width, n_layers, n_features):
+            super().__init__()
+            hidden = [nn.Linear(n_features, n_width)]
+            hidden += [nn.Linear(n_width, n_width) for _ in range(n_layers - 1)]
+            self.hidden = nn.ModuleList(hidden)
+            self.denselast = nn.Linear(n_width, 1)
+
+        def forward(self, x, **kwargs):
+            for layer in self.hidden:
+                x = layer(x)
+                x = nn.functional.sigmoid(x)
+            x = self.denselast(x)
+            return self.softmax(x)
+
+    def __init__(
+        self,
+        n_features: int = 10,
+        n_width: int = 5,
+        n_layers: int = 5,
+        loss_fn: Union[str, Callable] = "binary_cross_entropy_with_logits",
+        optimizer_fn: Union[str, Type[optim.Optimizer]] = "sgd",
+        lr: float = 1e-3,
+        output_is_logit: bool = True,
+        is_class_incremental: bool = False,
+        is_feature_incremental: bool = False,
+        device: str = "cpu",
+        seed: int = 42,
+        **kwargs,
+    ):
+        self.n_features = n_features
+        module = MultiLayerPerceptronInitialized.MLPModule(
+            n_width=n_width, n_layers=n_layers, n_features=n_features
+        )
         if "module" in kwargs:
             del kwargs["module"]
         super().__init__(
